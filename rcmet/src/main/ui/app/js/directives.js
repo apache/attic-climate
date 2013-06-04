2f57f48697e71c872a6088ccd5c52c21c31b7b9b
joyce <joyce@unknown>
2013-06-04 00:29:59 +0000

    Resolves CLIMATE-74 - Adds on-blur directive.
    - Adds the on-blur directive. The function that is passed with the
      on-blur attribute will be called under the enclosing scope when the
      tags blur event triggers.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1489255 13f79535-47bb-0310-9956-ffa450edef68

--- rcmet/src/main/ui/app/js/directives.js
+++ rcmet/src/main/ui/app/js/directives.js
@@ -371,4 +371,21 @@ directive('predictiveFileBrowserInput', function() {
 		scope: true,
 		restrict: 'A'
 	};
-});
+}).
+// The onBlur directive calls a passed function when a field's "blur" event is called.
+// The function should be passed as part of the "on-blur" attribute and be defined in
+// the containing scope.
+//
+// Consider the test function "testFunc". If you wanted this to run on the blur event
+// for an input box you would use the following:
+//   <input type="text" on-blur="testFunc();" />
+directive('onBlur', function() {
+	return {
+        restrict: 'A',
+        link: function($scope, $elem, $attrs) {
+            $elem.bind('blur', function() {
+				$scope.$eval($attrs.onBlur);
+			});
+        },
+    };
+ });
