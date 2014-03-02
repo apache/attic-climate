9300c236c67200b61e18a81d3d103ed8ada375aa
joyce <joyce@unknown>
2014-03-02 06:47:36 +0000

    Reverting accidentally committed debug code
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1573262 13f79535-47bb-0310-9956-ffa450edef68

--- ocw-ui/backend/tests/test_processing.py
+++ ocw-ui/backend/tests/test_processing.py
@@ -268,12 +268,12 @@ class TestRunEvaluation(unittest.TestCase):
         if not os.path.exists('d2.nc'):
             urlretrieve(FILE_LEADER + FILE_2, 'd2.nc')
 
-    #@classmethod
-    #def tearDownClass(self):
-        #if os.path.exists('d1.nc'):
-            #os.remove('d1.nc')
-        #if os.path.exists('d2.nc'):
-            #os.remove('d2.nc')
+    @classmethod
+    def tearDownClass(self):
+        if os.path.exists('d1.nc'):
+            os.remove('d1.nc')
+        if os.path.exists('d2.nc'):
+            os.remove('d2.nc')
 
     def test_full_evaluation(self):
         data = {
@@ -304,7 +304,6 @@ class TestRunEvaluation(unittest.TestCase):
             'temporal_resolution': 365,
             'metrics': ['Bias'],
             'start_time': '1989-01-01 00:00:00',
-            #'end_time': '2008-01-01 00:00:00',
             'end_time': '1991-01-01 00:00:00',
             'lat_min': -25.0,
             'lat_max': 22.0,
@@ -315,8 +314,7 @@ class TestRunEvaluation(unittest.TestCase):
 
         # NOTE: Sometimes the file download will die if you use the this WebTest
         # call for testing. If that is the case, download the files manually with wget.
-        #test_app.post_json('/processing/run_evaluation/', data)
-        bp.run_evaluation(data)
+        test_app.post_json('/processing/run_evaluation/', data)
         result_dirs = [x for x in os.listdir(WORK_DIR)
                        if os.path.isdir(os.path.join(WORK_DIR, x))]
 
